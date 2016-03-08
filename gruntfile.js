5e25c7916aafd966cd4dd22ad179b1f3c499f440
tongjinle <1328192478@qq.com>
2016-03-08 10:01:25 +0800

    改回使用js

--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,54 @@
-module.exports = function(grunt){
-	require('load-grunt-tasks')(grunt);
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-typescript');
+module.exports = function (grunt) {  
+	require("colors");
+	// show elapsed time at the end  
+	require('time-grunt')(grunt);  
+	// load all grunt tasks  
+	require('load-grunt-tasks')(grunt);  
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-jsdoc');
 
-	grunt.initConfig({
-		typescript:{
-			mgr:{
-				src:['script/**/*.ts','controller/**/*.ts','directive/script/**/*.ts','klass/**/*.ts','study/**/*.ts'],
-				// dest:'script/index.js',
-				options:{
-					module: 'amd', //or commonjs
-					target: 'es5', //or es3
-					// rootDir: 'path/to/typescript/files'
-					// rootDir: '/study2',
-					generateTsConfig:true,
-					 keepDirectoryHierarchy: true
-				}
-			}
-		},
-		connect:{
-				options:{
-					port: 9001,
-					livereload: 35731,
-					// change this to '0.0.0.0' to access the server from outside
-					hostname: 'localhost'
-					// , keepalive:true
-				},
-				livereload:{
-					options:{
-						open: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/index.html',
-					}
-				},
-				mgr:{
-					options:{
-						open: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/main.html',
-					}
-				}
-		},
-		watch:{
-			options: {
-				livereload: '<%= connect.options.livereload %>',
-				debounceDelay: 250,
-			},
-			mgr:{
-				files:['css/bundle.css','script/main.js','**/*.html','declare/**/*.d.ts']
-			},
-			ts:{
-				files:'<%= typescript.mgr.src %>',
-				tasks:[
-					'typescript:mgr'
-				]
+	grunt.initConfig({  
+		watch: {  
+			livereload: {  
+				options: {  
+					livereload: '<%= connect.options.livereload %>'    
+				},  
+				files: [
+					'main.html',
+					'seaconfig.js',
+					'view/**/*.{html,htm}', 
+					'directive/**/*.{html,htm}', 
+					'script/**/*.js', 
+					'css/bundle.css',
+					'service/dist/*.js',
+					'controller/dist/*.js',
+					'<%= jsdoc.dist.options.destination%>/*.html'
+				]  
 			},
 			less:{
-				files:['less/**/*.less','directive/less/**/*.less'],
-				tasks:['less']
+				files:[
+					'less/**/*.less',
+					'directive/less/**/*.less'
+				],
+				tasks:["less"]
+			},
+			directive:{
+				files:['directive/script/**/*.js'],
+				tasks:["concat"]
+			},
+			service:{
+				files:['service/script/**/*.js'],
+				tasks:["concat"]
+			},
+			controller:{
+				files:['controller/script/**/*.js'],
+				tasks:["concat"]
+			},
+			// jsdoc
+			genDoc:{
+				files:['<%= jsdoc.dist.src%>'],
+				tasks:['jsdoc']
 			}
 		},
 		less:{
@@ -65,19 +58,84 @@ module.exports = function(grunt){
 					"css/bundle.css":'<%= watch.less.files %>'
 				}
 			}
+		} ,
+		connect: {  
+			options: {  
+				port: 9001,  
+				livereload: 35731,  
+				// change this to '0.0.0.0' to access the server from outside  
+				hostname: 'localhost'  
+			},  
+			livereload: {  
+				options: {  
+					open: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/main.html',  
+				}  
+			},
+			genDoc:{
+				options:{
+					open:'http://<%= connect.options.hostname %>:<%= connect.options.port %>/<%= jsdoc.dist.options.destination%>/index.html'
+				}
+			}  
+		},
+		concat:{
+			options: {
+			  separator: ';',
+			},
+			directive: {
+			  // src: ['directive/script/**/*.js'],
+			  src: '<%= watch.directive.files %>',
+			  dest: 'directive/dist/directive-bundle.js',
+			},
+			service:{
+			  // src: ['service/script/**/*.js'],
+			  src: '<%= watch.service.files %>',
+			  dest: 'service/dist/service-bundle.js',
+			},
+			controller:{
+			  src: '<%= watch.controller.files %>',
+			  dest: 'controller/dist/controller-bundle.js',
+			}
+		} ,
+		jsdoc: {
+			dist: {
+				src: ['service/script/*.js','script/*.js'],
+				options: {
+					destination: 'doc',
+					   template : "node_modules/ink-docstrap/template",
+					  configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
+				}
+			}
 		}
+	});  
+
+
+	grunt.registerTask("lessChangedAlert",function(){
+		console.log("compile less file ... ".green);
 	});
 
-	grunt.registerTask('default',
-		[
-			'typescript:mgr',
-			'connect:mgr',
+	grunt.registerTask('compile',function(){
+		grunt.task.run([
+			'lessChangedAlert',
 			'less',
+			'concat'
+		]);
+	});
+  
+	grunt.registerTask('serve', function () {  
+		grunt.task.run([  
+			'compile',  
+			'connect:livereload',  
 			'watch'
-		]
-	);
+		]);  
+	});  
 
-
-	grunt.registerTask('ts',['typescript:mgr']);
-
-};
+	grunt.registerTask('genDoc',function(){
+		grunt.task.run([
+			'jsdoc',
+			'connect:genDoc',
+			'watch'
+		]);
+	});
+  
+	grunt.registerTask('default', ['serve']);  
+};  
