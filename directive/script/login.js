7d9db045059833ebf785ac152a55dd28ca314eb1
wangcongcong <740707701@qq.com>
2016-03-15 09:40:29 +0800

    增加 用户注册api (未完成)

--- directive/script/login.js
+++ directive/script/login.js
@@ -5,16 +5,42 @@
 * @Last Modified time: 2016-03-10 10:27:24
 */
 
-define(["app"],function(app){
-    app.directive("lmlogin",function(){
+define(["app",
+		"cookie",
+		"service-user"
+	],function(app,cookie){
+    app.directive("lmlogin",["lmUserService",function(userService){
             return {
-                        restrict:"E",
-                        templateUrl:"/directive/html/login.html",
-                        link:function($scope,$element,$attrs){
+                restrict:"E",
+                templateUrl:"/directive/html/login.html",
+                link:function($scope,$element,$attrs){
+                	$scope.username = userService.token().username;
+                	$scope.password = userService.token().password;
 
-                        }
-                    }
+                	//注册
+                	this.register = function(mobile,email,pwd,captcha){
+                		userService.regiser()
+                		.success(function(data){
+                			console.log(data);
+                		})
+                		.error(function(err,data){
+                			console.log("error",err);
+                		})
+                	};
 
-    });
+                	//登录
+                	$scope.login = function(){
+                		userService.login($scope.username,$scope.password)
+                		.success(function(data){
+                			console.log("login",data);
+                		})
+                		.error(function(err,data){
+                			alert("error");
+                		});
+                	}
+                }
+            }
+
+    }]);
 
 });
--- service/script/user.js
+++ service/script/user.js
@@ -4,10 +4,13 @@ define([
 	'cookie'
 	],
 	function(app,cookie){
-		app.service('lmUserService', ['$http',function($http){
+		app.service('lmUserService', ['$http',"md5",function($http,md5){
 			var urlPrefix = 'http://192.168.1.240:8002';
 			var urlDict = {
-				login:'/oauth/access_token'
+				login:'/lingmall/oauth/access_token',
+				register:"/lingmall/users/register",
+				sendmobilecode:"/lingmall/sms/send",
+				validationcode:" /lingmall/users/captcha"
 			};
 			var methodDict = {
 				post:'POST',
@@ -15,18 +18,66 @@ define([
 			};
 
 			// 我的商品库
-			this.login = function(username,password){
+			this.login = function(username,pwd){
 				return $http({
-					url:urlDict.goods,
+					url:urlPrefix+urlDict.login,
 					method:methodDict.post,
 					data:{
 						"username":username,
-						"password":password,
+						"password":md5.createHash(pwd),
 						"grant_type":"password",
 						"client_id":"f3d259ddd3ed8ff3843839b",
 						"client_secret":"4c7f6f8fa93d59c45502c0ae8c4a95b"
 					}
 				});
+			};
+
+			//注册
+			this.register = function(mobile,email,pwd,captcha){
+				return $http({
+					url:urlPrefix + urlDirt.register,
+					method:methodDict.post,
+					data:{
+						"moblie":mobile,
+						"email":eamil,
+						"password":md5.createHash(pwd),
+						"captcha":captcha
+					}
+				});
+			};
+
+			/*"account": "18637633325",
+  			"type": "1=>注册; 2=>重置密码"*/
+
+			//发送手机验证码
+			this.sendmobilecode = function(account,type){
+				return $http({
+					url:urlPrefix + urlDirt.sendmobilecode,
+					method:methodDict.post,
+					data:{
+						"account":account,
+						"type":type
+					}
+				})
+			};
+
+			/*{
+			  "captcha": "123456",
+			  "mobile": "15618688068",
+			  "type": "1=>注册; 2=>重置密码"
+			}*/
+
+			//验证 验证码
+			this.validationcode = function(captcha,mobile,type){
+				return $http({
+					url:urlPrefix + urlDirt.validationcode,
+					method:methodDict.post,
+					data:{
+						"captcha":captcha,
+						"mobile":mobile,
+						"type":type
+					}
+				})
 			};
 
 			this.token = function(token){
