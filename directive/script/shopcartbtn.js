2fad5c9568b0336488f019379740a26aefdc2b35
tongjinle <1328192478@qq.com>
2016-03-21 10:57:21 +0800

    数据中心的购物车按钮,点击之后转到购物车页面

--- directive/script/shopcartbtn.js
+++ directive/script/shopcartbtn.js
@@ -1,11 +1,13 @@
-define(["app"],function(app){
-	app.directive("lmshopcartbtn",function(){
+define(["app",
+	'service-util'
+	],function(app){
+	app.directive("lmshopcartbtn",['lmUtilService',function(utilService){
 		return {
 			restrict:"E",
 			templateUrl:"../directive/html/shopcartbtn.html",
 			link:function($scope,$element,$attrs){
-				
+				$scope.linkTo = utilService.linkTo;
 			}
 		}
-	});
+	}]);
 });
