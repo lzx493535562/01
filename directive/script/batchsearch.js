e8945b3e993f7d3da821f9fe4cfc5454a26e3358
wangcongcong <740707701@qq.com>
2016-03-22 17:21:47 +0800

    添加 结算 支付

--- directive/script/batchsearch.js
+++ directive/script/batchsearch.js
@@ -1,5 +1,7 @@
-define(["app"],function(app){
-	app.directive("lmbatchsearch",function(){
+define(["app",
+		"service-user"
+	],function(app){
+	app.directive("lmbatchsearch",["lmUserService",function(userService){
 		return {
 			restrict:"E",
 			templateUrl:"../directive/html/batchsearch.html",
@@ -9,7 +11,38 @@ define(["app"],function(app){
 				$scope.goback = function(){
 					$scope.$emit("goback");
 				};
+
+				// 结算
+				/*$scope.confirm = function(){
+					var callbackUrl = window.location.href;
+					//console.log("callbackUrl",callbackUrl);
+
+					var fn = function(){
+						var action = 'http://192.168.1.240:88/lingmall/alipay';
+						var form = $('<form/>').attr({
+							action:action,
+							method:'post',
+							enctype:'application/json',
+							target:'_blank'
+						});
+
+
+						var appendValue = function(name,value){
+							var input = $('<input/>').attr('name',name).val(value);
+							form.append(input);
+						};
+						appendValue('access_token',userService.token());
+						appendValue('order_number',$scope.selectedCount);
+						appendValue('order_price',$scope.selectedPrice);
+						appendValue('callback_url',callbackUrl);
+
+						form[0].submit();
+					};
+
+					fn();
+				};*/
+
 			}
 		}
-	});
+	}]);
 });
